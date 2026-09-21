c9921935701dc14624f9a5c6d93e1fae628dadfb
Derek Oliveira <derek.foliveira@gmail.com>
2026-09-21 20:23:27 -0300

    Bottom navigation tabs

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,13 +1,21 @@
-import { View, Text } from 'react-native'
-import { Link } from 'expo-router'
+import { View, Text } from "react-native";
+import { Link } from "expo-router";
 
 const SignIn = () => {
   return (
     <View>
-        <Text>Sign In</Text>
-        <Link href="/(auth)/sign-up" className="mt-4 rounded justify-center items-center bg-primary text-white py-4">Create account</Link>
+      <Text>Sign In</Text>
+      <Link
+        href="/(auth)/sign-up"
+        className="mt-4 rounded justify-center items-center bg-primary text-white py-4"
+      >
+        Create account
+      </Link>
+      <Link href="/" className="mt-4 rounded bg-primary text-white p-4">
+        Go Back
+      </Link>
     </View>
-  )
-}
+  );
+};
 
-export default SignIn
+export default SignIn;
